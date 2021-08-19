067195cc04f34ef3412aa3d5f4ddcb3fb5124a92
Chris <Chris@margaritas-air.attlocal.net>
2021-08-19 08:42:12 -0700

    crud routes complete and tested on postman

--- controllers/controller.js
+++ controllers/controller.js
@@ -9,6 +9,7 @@ const Song = require("../models/Song");
 const songSeed = require("../db/seedData.json");
 
 // ROUTES
+// SEED
 router.get("/seed", async (req,res) => {
     try {
         await Song.remove({});
@@ -19,5 +20,49 @@ router.get("/seed", async (req,res) => {
         res.status(400).json(error);
     }
 });
+
+// INDEX
+router.get("/", async (Req, res) => {
+    try {
+      const songs = await Song.find({});
+      res.json(songs);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  });
+
+// CREATE 
+router.post("/", async (req, res) => {
+    try {
+      const newSong = await Song.create(req.body);
+      res.json(newSong);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  });  
+
+// UPDATE
+router.put("/:id", async (req, res) => {
+    try {
+      const updatedSong = await Song.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+      );
+      res.json(updatedSong);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  });
+
+// DELETE
+router.delete("/:id", async (req, res) => {
+    try {
+      const deletedSong = await Song.findByIdAndRemove(req.params.id);
+      res.json(deletedSong);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  });
 
 module.exports = router; 
